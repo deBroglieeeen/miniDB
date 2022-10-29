c637555dfa0e02a2be6aa2d29f246c1bfc7321b7
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2022-10-29 17:06:49 +0900

    add Disk manager

---
+++ src/disk.rs
@@ -0,0 +1,21 @@
+pub struct DiskManager {
+	heap_file: File,
+	next_page_id: u64
+}
+
+impl DiskManager {
+	pub fn new(data_file: File) -> io::Result<Self> {
+
+	}
+
+	pub fn open(data_file_path: impl AsRef<Path>)
+	    -> io::Result<zself> {
+
+	    }
+
+	pub fn allocate_page(&mut self)-> PageId {
+
+	}
+
+	pub fn read_page_data(&mut self, page_id)
+}
